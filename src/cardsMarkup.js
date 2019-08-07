5fcbfad0ca60186647fe1fbb4f11dab2dcea11ab
Alex Masterov <alex.masterow@gmail.com>
2019-08-08 00:17:43 +0300

    Fix render tags
    Also fix CS

--- src/cardsMarkup.js
+++ src/cardsMarkup.js
@@ -14,6 +14,33 @@ const timeFormat = new Intl.DateTimeFormat('en-GB', {
 const formatDate = (date) => dateFormat.format(date).toUpperCase();
 const formatTime = (date) => timeFormat.format(date);
 
+const getTextArea = (text) => `
+  <div class="card__textarea-wrap">
+    <p class="card__text">${text}</p>
+  </div>`.trim();
+
+const getDateMarkup = (date) => `
+  <div class="card__dates">
+    <div class="card__date-deadline">
+      <p class="card__input-deadline-wrap">
+        <span class="card__date">${formatDate(date)}</span>
+        <span class="card__time">${formatTime(date)}</span>
+      </p>
+    </div>
+  </div>`;
+
+const getHashTagMarkup = (name) => `
+  <span class="card__hashtag-inner">
+    <span class="card__hashtag-name">${name}</span>
+  </span>`.trim();
+
+const getHashTagListMarkup = (tags) => `
+  <div class="card__hashtag">
+    <div class="card__hashtag-list">
+      ${tags.map(getHashTagMarkup).join('\n')}
+    </div>
+  </div>`.trim();
+
 const getCardMarkup = ({text, date, tags = []} = {}) => `
   <article class="card">
     <div class="card__form">
@@ -39,52 +66,15 @@ const getCardMarkup = ({text, date, tags = []} = {}) => `
         </div>
 
         ${getTextArea(text)}
+
         <div class="card__settings">
           <div class="card__details">
-            ${getDateMarkup(date, time)}
-            ${tags.length > 0 ? getHashTagList(tags) : ``}
+            ${getDateMarkup(date)}
+            ${tags.length > 0 ? getHashTagListMarkup(tags) : ``}
           </div>
         </div>
       </div>
     </div>
   </article>`;
 
-
-const getTextArea = (cardText) => {
-  return `
-  <div class="card__textarea-wrap">
-  <p class="card__text">${cardText}</p>
-  </div>`;
-};
-
-
-const getDateMarkup = (date) => `
-  <div class="card__dates">
-    <div class="card__date-deadline">
-      <p class="card__input-deadline-wrap">
-        <span class="card__date">${formatDate(date)}</span>
-        <span class="card__time">${formatTime(date)}</span>
-      </p>
-    </div>
-  </div>`;
-
-const hashTagMarkup = (tagsList = []) => `
-<span class="card__hashtag-inner">
-  <span class="card__hashtag-name">
-    ${tagsList};
-  </span>
-</span>
-`;
-
-const getHashTagList = (tagsList) => {
-  return `
-<div class="card__hashtag">
-<div class="card__hashtag-list">
-  ${tagsList.map(hashTagMarkup).join(`\n`)}
-</div>
-</div>
-`;
-};
-
-
-window.cardsMarkup = window.data.cardData.map(getCardMarkup).join(`\n`);
+window.cardsMarkup = window.data.cardData.map(getCardMarkup).join(`\n`)
